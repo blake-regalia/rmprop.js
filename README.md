656cec55a89db698f3a430b017289121308e0ad4
Blake Regalia <blake.regalia@gmail.com>
2015-11-02 01:37:04 -0800

    stable

--- README.md
+++ README.md
@@ -1,32 +1,55 @@
-# ldquery [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
-> Query linked-data graphs by abstracting away traditional JSON-LD interaction
+# rmprop [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> 
+
+
+If you've ever wanted to use property names for other purposes such as the `.length` property on an `Array` or `String`, this library will transform your object (eg: an instance of a class) into an object with all properties found in its prototype chain set to `undefined`, effectively stopping the javascript engine from traversing higher up the prototype chain to find the values for those properties. 
 
 
 ## Install
 
 ```sh
-$ npm install --save ldquery
+$ npm install --save rmprop
 ```
 
 
 ## Usage
 
-```js
-var ldquery = require('ldquery');
+### rmprop(thing: anytype[, exclude: array{string}])
 
-ldquery();
+The optional `exclude` argument declares which properties to not override.
+
+```js
+var rmprop = require('rmprop');
+
+var array = rmprop(['hello','there', ['indexOf']); // override all array properties except for the .indexOf method
+
+array.length; // undefined
+array[0]; // 'hello'; because this property is defined on the object itself, not in Array.prototype
+array.indexOf('there'); // 1
+array.join; // undefined
+array.reverse; // undefined
+
+var str = rmprop('hi', ['valueOf']); // do not override .valueOf
+(str == 'hi'); // true; loose equality operator uses valueOf function
+(str === 'hi'); // false; str type is a String object
+str.length; // undefined
+str.__proto__; // undefined
+str[rmprop.real] === 'hi'; // true
 ```
+
+### Primitive datatypes
+Primitive datatypes will be boxed into their object equivalent (eg: `25` becomes `Number(25)`) so that they can hold properties. This also means that you need to 
 
 ## License
 
 ISC © [Blake Regalia]()
 
 
-[npm-image]: https://badge.fury.io/js/ldquery.svg
-[npm-url]: https://npmjs.org/package/ldquery
-[travis-image]: https://travis-ci.org/blake-regalia/ldquery.svg?branch=master
-[travis-url]: https://travis-ci.org/blake-regalia/ldquery
-[daviddm-image]: https://david-dm.org/blake-regalia/ldquery.svg?theme=shields.io
-[daviddm-url]: https://david-dm.org/blake-regalia/ldquery
-[coveralls-image]: https://coveralls.io/repos/blake-regalia/ldquery/badge.svg
-[coveralls-url]: https://coveralls.io/r/blake-regalia/ldquery
+[npm-image]: https://badge.fury.io/js/rmprop.svg
+[npm-url]: https://npmjs.org/package/rmprop
+[travis-image]: https://travis-ci.org/blake-regalia/rmprop.svg?branch=master
+[travis-url]: https://travis-ci.org/blake-regalia/rmprop
+[daviddm-image]: https://david-dm.org/blake-regalia/rmprop.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/blake-regalia/rmprop
+[coveralls-image]: https://coveralls.io/repos/blake-regalia/rmprop/badge.svg
+[coveralls-url]: https://coveralls.io/r/blake-regalia/rmprop
--- test/index.js
+++ test/index.js
@@ -14,6 +14,13 @@ describe('unprop', function () {
 		assert.deepEqual(b.__proto__);
 	});
 
+	it('supports exclusion for booleans', () => {
+		let b = unprop(false, ['valueOf', 'toString']);
+
+		assert.deepEqual(b==false, true);
+		assert.deepEqual(b+'', 'false');
+	});
+
 	it('supports numbers', () => {
 		let n = unprop(10);
 
@@ -21,6 +28,13 @@ describe('unprop', function () {
 		assert.deepEqual(n.valueOf);
 		assert.deepEqual(n.toString);
 		assert.deepEqual(n.__proto__);
+	});
+
+	it('supports exclusion for numbers', () => {
+		let n = unprop(10, ['valueOf', 'toString']);
+
+		assert.deepEqual(n == 10, true);
+		assert.deepEqual(n+'', '10');
 	});
 
 	it('supports strings', () => {
@@ -34,6 +48,13 @@ describe('unprop', function () {
 		assert.deepEqual(s.__proto__);
 	});
 
+	it('supports exclusion for strings', () => {
+		let s = unprop('hi', ['valueOf', 'toString', 'length']);
+
+		assert.deepEqual(s == 'hi', true);
+		assert.deepEqual(s+'', 'hi');
+		assert.deepEqual(s.length, 2);
+	});
 
 	it('supports objects', () => {
 		let o = unprop({test: 'hi'});
@@ -41,6 +62,14 @@ describe('unprop', function () {
 		assert.deepEqual(o.test, 'hi');
 		assert.deepEqual(o.hasOwnProperty);
 		assert.deepEqual(o.toString);
+	});
+
+	it('supports exclusion for objects', () => {
+		let o = unprop({test: 'hi'}, ['valueOf']);
+
+		assert.deepEqual(o.test, 'hi');
+		assert.deepEqual(o.toString);
+		assert.deepEqual(o.valueOf(), o);
 	});
 
 	it('supports functions', () => {
@@ -53,38 +82,6 @@ describe('unprop', function () {
 		assert.deepEqual(f.__proto__);
 	});
 
-	it('supports exclusion for booleans', () => {
-		let b = unprop(false, ['valueOf', 'toString']);
-
-		assert.deepEqual(b==false, true);
-		assert.deepEqual(b+'', 'false');
-	});
-
-
-	it('supports exclusion for numbers', () => {
-		let n = unprop(10, ['valueOf', 'toString']);
-
-		assert.deepEqual(n == 10, true);
-		assert.deepEqual(n+'', '10');
-	});
-
-	it('supports exclusion for strings', () => {
-		let s = unprop('hi', ['valueOf', 'toString', 'length']);
-
-		assert.deepEqual(s == 'hi', true);
-		assert.deepEqual(s+'', 'hi');
-		assert.deepEqual(s.length, 2);
-	});
-
-
-	it('supports exclusion for objects', () => {
-		let o = unprop({test: 'hi'}, ['valueOf']);
-
-		assert.deepEqual(o.test, 'hi');
-		assert.deepEqual(o.toString);
-		assert.deepEqual(o.valueOf(), o);
-	});
-
 	it('supports exclusion for functions', () => {
 		let f = unprop(() => {
 			return 'hi';
@@ -92,6 +89,40 @@ describe('unprop', function () {
 
 		assert.deepEqual(typeof f.bind, 'function');
 		assert.deepEqual(typeof f.apply, 'function');
+	});
+
+	it('supports arrays', () => {
+		let a = unprop(['hi']);
+
+		assert.deepEqual(a[0], 'hi');
+		assert.deepEqual(a.length);
+	});
+
+	it('supports exclusion for arrays', () => {
+		let a = unprop(['hi'], ['indexOf']);
+
+		assert.deepEqual(a[0], 'hi');
+		assert.deepEqual(a.indexOf('hi'), 0);
+	});
+
+
+	it('allows access to real entity', () => {
+		let s = unprop('hi');
+
+		assert.deepEqual(s.length);
+		assert.deepEqual(s[unprop.real], 'hi');
+		assert.deepEqual(s[unprop.real].length, 2);
+	});
+
+	it('allows new methods to use real object', () => {
+		let a = unprop(['hi','there']);
+
+		Object.defineProperty(a, 'length', {
+			get: function() {
+				return this[unprop.real].join('').length;
+			},
+		});
+		assert.deepEqual(a.length, 7);
 	});
 
 });
